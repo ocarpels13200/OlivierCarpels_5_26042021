30ac47d94a89824720f3678437ff1717f579671e
olivier <olivier@ocarpels.fr>
2021-11-30 15:33:28 +0100

    Creation de la bulle nombre d'objet, travail sur product.js et commentaires

--- publics_html/js/basket.js
+++ publics_html/js/basket.js
@@ -1,2 +1,102 @@
-let documentURL = document.location.search;
-console.log(documentURL);
+/*
+La fonction numItem génère le nombre de produits dans le panier pour l'afficher dans la barre de navigation
+*/
+numItem()
+
+/*
+Génration de variables globales :
+----localStorageItems récupère sur le localStorage les produits présents
+----localStorageItem transforme le contenu de la variable précédente pour être lisible par le Javascript
+----basket permet de repérer sur la page HTML le tableau récapitulant les produits achetés
+----totalPricePerItem représente le prix total pour un produit donné -> (prix produit * quantité)
+----totalPrice représente le prix total de l'ensemble des produits -> (Somme des totalPricePerItem)
+*/
+let localStorageItems = localStorage.getItem("product");
+let localStorageItem = JSON.parse(localStorageItems);
+let basket = document.getElementById("basket");
+let totalPricePerItem;
+let totalPrice = 0;
+
+/*
+La condition ci-dessous permet de vérifier la présence ou non de produit dans le localStorage.
+lignes 25 à 45 -> En cas de présence de produits, par le biais d'une boucle nous mettons à jour les prix par produits et le prix total. Nous complétons également le fichier HTML avec les bonnes informations.
+lignes 46 à 48 -> Nous indiquons en cas de non présence de produits dans le localStorage que le panier est vide.
+*/
+if (localStorageItem !==null){
+    for (let i = 0; i < localStorageItem.length; i++){
+        totalPricePerItem = parseInt((localStorageItem[i].price/100) * (localStorageItem[i].quantity));
+        totalPrice = parseInt(totalPrice + totalPricePerItem);
+        basket.innerHTML += '<tr>' +
+            '<td class="text-nowrap">' + localStorageItem[i].name + '</td>' +
+            '<td class="px-5 text-end text-nowrap">' + localStorageItem[i].price/100 + ' €</td>' +
+            '<td class="text-center text-nowrap"><a class="btn btn-outline-primary btn-sm me-3 deleteProduct">-</a>' + localStorageItem[i].quantity + '<a class="btn btn-outline-primary btn-sm ms-3 addProduct">+</a></td>' +
+            '<td class="text-end text-nowrap">' + totalPricePerItem + ' €</td>' +
+            '<td class="text-center text-nowrap"><a class="btn btn-outline-primary btn-sm deleteProducts"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-x" viewBox="0 0 16 16">\n' +
+            '  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>\n' +
+            '</svg></a></td>' +
+            '</tr>';
+    }
+    basket.innerHTML += '<tr>' +
+        '<td></td>' +
+        '<td></td>' +
+        '<td class="text-end fw-bold text-nowrap">Total TTC :</td>' +
+        '<td class="text-end text-nowrap">' + totalPrice + ' €</td>' +
+        '<td></td>' +
+        '</tr>';
+} else {
+    basket.innerHTML = '<h2 class="h5 text-center text-danger m-5">Votre panier est vide</h2>';
+}
+
+/*
+lignes 53 à 64 -> ces lignes permettent sur un clique sur le buton "X" de supprimer un produit sur le panier et de le supprimer du localStorage
+*/
+let deleteProducts = document.getElementsByClassName("deleteProducts");
+for (let i = 0; i < deleteProducts.length; i++){
+    deleteProducts[i].addEventListener("click", evt => {
+        localStorageItem.splice(localStorageItem[i], 1);
+        localStorage.setItem("product", JSON.stringify(localStorageItem));
+        if (localStorageItem.length === 0){
+            localStorage.clear();
+        }
+        numItem();
+        window.location.reload();
+    })
+}
+
+/*
+lignes 70 à 78 -> Ces lignes permettent d'ajouter pour un produit donné un élément à la quantité
+La fonction numItem met à jour la bulle dans la barre de navigation
+*/
+let addProduct = document.getElementsByClassName("addProduct");
+for (let i = 0; i < addProduct.length; i++){
+    addProduct[i].addEventListener("click", evt => {
+        localStorageItem[i].quantity++;
+        localStorage.setItem("product", JSON.stringify(localStorageItem));
+        numItem();
+        window.location.reload();
+    })
+}
+
+/*
+Lignes 85 à 101 -> Cest lignes permettent de supprimer pour un produit donné un élément à la quantité.
+Si, en supprimant un élément nous arrivons à zéro alors nous supprimons l'objet du panier et du localStorage.
+La fonction numItem met à jour la bulle dans la barre de navigation
+*/
+let deleteProduct = document.getElementsByClassName("deleteProduct");
+for (let i = 0; i < deleteProduct.length; i++){
+    deleteProduct[i].addEventListener("click", evt => {
+        localStorageItem[i].quantity--;
+        if (localStorageItem[i].quantity===0){
+            localStorageItem.splice(localStorageItem[i], 1);
+            localStorage.setItem("product", JSON.stringify(localStorageItem));
+            if (localStorageItem.length === 0){
+                localStorage.clear();
+            }
+        } else {
+            localStorage.setItem("product", JSON.stringify(localStorageItem));
+        }
+        numItem();
+        window.location.reload();
+    })
+}
+
--- publics_html/js/product.js
+++ publics_html/js/product.js
@@ -36,7 +36,7 @@ function getProduct () {
 }
 
 /*
-La fonction objectDescription permet grâce à innerHTML d'alimenter la page product.html avec les information du produit choisi
+La fonction objectDescription permet grâce à innerHTML d'alimenter la page product.html avec les informations du produit choisi
 */
 function objectDescription(productSelection){
    const productPicture = document.querySelector("#description img");
@@ -55,25 +55,58 @@ function objectDescription(productSelection){
 }
 
 /*
-À partir d'ici, nous générons un événement sur un click sur le bouton ajouter au panier
-Dans la fonction à flèche, nous récupérons le contenu du localstorage pour vérifier si un produit y est déjà présent.
-Si le localStorage n'est pas vide, nous poussons le produit choisi dans selui-ci
-Si le localStorage est vide, alors nous pourssons le produit choisi dans le tableau product et nous l'envoyons dans le localstorage
-Enfin nous lançons la fonction numitem pour générer le nombre de produits dans le panier
+Lignes 79 et 80 -> Nous générons un événement click sur le bouton ajouter au panier et une fonction à fleche.
+
+    Lignes 81 et 82 -> À partir de la troisième ligne, nous testons, à l'aide de la variable productQuantity, l'input quantité du site pour s'assurer qu'une quantité, au minimum de 1, à bien été mise.
+    Lignes 105 à 107 -> Si nous n'avons pas choisi de quantité sur le site nous alertons l'utilisateur
+    Ligne 82 -> Si la condition est bonne :
+
+        Ligne 83 -> En 5ème ligne, nous ajoutons au produit choisi la quantité mise sur le site
+
+            Lignes 84 à 86 -> Nous vérifions dans le localStorage la présence de produits.
+            Lignes 101 à 104 -> Si le localStorage est vide, à l'aide de la variable product (array) j'alimente pour la première fois le localStorage
+
+                Lignes 88 à 92 -> À l'aide d'une boucle nous testons l'ID de l'ensemble des produits présents dans le localStorage par rapport au produit choisi sur le site pour y vérifier sa présdence.
+
+                Lignes 87, 90 et 93 à 95 -> En cas de réponse positive, nous récupérons dans une variable productPrésent l'index du produit dans le localStorage et nous mettons à jour la quantité de produits sachetés dans le localStorage. Enfin nous renvoyons le tout dans le localStorage (lignes 15 à 17)
+
+                Lignes 96 à 99 -> Si le nouveau produit choisi n'est pas présent dans le localStorage, alors nous lui ajoutons le nouveau produit.
+
+Ligne 109 -> Nous mettons en route la fonction numItem pour mettre à jour le visuel dans la barre de navigation
+
+Ligne 110 ->2
 */
+
 let addBasket = document.getElementById("addBasket");
 addBasket.addEventListener("click", ev => {
-    currentProduct.quantity = parseInt(document.querySelector('#quantite').value);
-    let localStorageItems = localStorage.getItem("product");
-    let localStorageItem = JSON.parse(localStorageItems);
-    if (localStorageItem !== null){
+    let productquantity = parseInt(document.querySelector('#quantite').value);
+    if (productquantity >= 1){
+        currentProduct.quantity = productquantity;
+        let localStorageItems = localStorage.getItem("product");
+        let localStorageItem = JSON.parse(localStorageItems);
+        if (localStorageItem !== null){
+            let productPresent = null;
+            for (let i = 0; i < localStorageItem.length; i++ ){
+                if (currentProduct._id === localStorageItem[i]._id){
+                    productPresent = i;
+                }
+            }
+            if (productPresent !== null){
+                localStorageItem[productPresent].quantity += currentProduct.quantity;
+                localStorage.setItem("product", JSON.stringify(localStorageItem));
+            } else {
+                localStorageItem.push(currentProduct);
+                localStorage.setItem("product", JSON.stringify(localStorageItem));
+            }
 
-        localStorageItem.push(currentProduct);
-        localStorage.setItem("product", JSON.stringify(localStorageItem));
-
+        } else {
+            product.push(currentProduct);
+            localStorage.setItem("product", JSON.stringify(product));
+        }
     } else {
-        product.push(currentProduct);
-        localStorage.setItem("product", JSON.stringify(product));
+        alert("Vous devez àjouter au minimum un produit au panier")
     }
+
     numItem();
+    window.location.reload();
 });
